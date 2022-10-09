95fe0b10bd36d81aeb09852694c7de64b2a1ff58
Matheus Patriota <mpatriota55@gmail.com>
2022-10-09 20:28:55 -0300

    fix: style colors changed

--- src/components/Puzzle/index.tsx
+++ src/components/Puzzle/index.tsx
@@ -6,7 +6,7 @@ export function Puzzle({ values }: any) {
       <div className="flex max-w-[200px] flex-wrap justify-center items-center">
         {values.map((value: any, index: any) => (
           <div
-            className="w-[40px] h-[40px] bg-gray-400 flex items-center justify-center m-2"
+            className="w-[40px] h-[40px] bg-[rgb(30,41,59)] flex items-center justify-center m-2 rounded-sm"
             key={index}
           >
             {value}
